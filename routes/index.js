ed62ab01954bca4ea0d057bec513bf0bf9ddfb02
AntoAntonyCyriac <antoantonycyriac@gmail.com>
2024-03-30 00:55:06 +0530

    ADDING AUTHENTICATION

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import doctorRoutes from "./doctorRoutes/index.js";
+import userRoutes from "./userRoutes/index.js";
 import departmentRoutes from "./departmentRoutes/index.js";
 import imageRoute from "./imageRoute/index.js";
 
@@ -7,6 +8,7 @@ const router = express.Router();
 
 router.use("/doctor", doctorRoutes);
 router.use("/department", departmentRoutes);
+router.use("user", userRoutes);
 router.use("/upload", imageRoute);
 
 export default router;
